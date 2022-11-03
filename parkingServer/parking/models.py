c27836b445f2a9fb6bb81d0da48e14a73dc4552e
Gavin Murambadoro <Developer.Gavin@gmail.com>
2022-11-03 13:51:20 +0200

    Add model support for gates

--- parkingServer/parking/models.py
+++ parkingServer/parking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+DIRECTION_CHOICES = [
+    ('NONE', 'NONE'),
+    ('IN', 'IN'),
+    ('OUT', 'OUT'),
+    ('IN_OUT', 'IN & OUT'),
+]
+
+
+class Gate(models.Model):
+    """Gate represents an entry or exit configuration"""
+    name = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    direction = models.CharField(max_length=10, choices=DIRECTION_CHOICES,
+                                 default=DIRECTION_CHOICES[0][0], null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name} : {self.get_direction_display()}'
--- parkingServer/parkingServer/settings.py
+++ parkingServer/parkingServer/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
 
     # local apps
+    'parking',
 ]
 
 MIDDLEWARE = [
