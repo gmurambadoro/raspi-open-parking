6274a3f92233b9bef3f241d0a6b91bd6874e492a
Gavin Murambadoro <Developer.Gavin@gmail.com>
2022-11-03 15:52:53 +0200

    Adding movement support

--- parkingServer/parking/admin.py
+++ parkingServer/parking/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Gate
+from .models import Gate, Movement
 
 
 @admin.register(Gate)
 class GateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'direction')
-    search_fields = ('name', 'direction')
+    list_display = ('name', 'direction',)
+    search_fields = ('name', 'direction',)
     list_filter = ('direction',)
 
+
+@admin.register(Movement)
+class MovementAdmin(admin.ModelAdmin):
+    list_display = ('reference_number', 'entrance_datetime', 'entrance_gate', 'exit_gate', 'exit_datetime', 'status',)
+    search_fields = ('reference_number', 'entrance_gate__name', 'exit_gate__name',)
--- parkingServer/parking/models.py
+++ parkingServer/parking/models.py
@@ -1,3 +1,6 @@
+import datetime
+import uuid
+
 from django.core import validators
 from django.db import models
 
@@ -41,3 +44,27 @@ class Gate(models.Model):
 
     def __str__(self):
         return f'{self.name} : {self.get_direction_display()}'
+
+
+class Movement(models.Model):
+    """Represents an entry or exit via any one of the configured gates"""
+    reference_number = models.CharField(max_length=50, unique=True, null=False, blank=False,
+                                        editable=False,
+                                        default=lambda: str(uuid.uuid4()),
+                                        verbose_name='Barcode')
+    entrance_gate = models.ForeignKey(Gate, on_delete=models.CASCADE, null=False, related_name='entrance_gates')
+    entrance_datetime = models.DateTimeField(null=False, blank=False, default=datetime.datetime.now)
+    exit_gate = models.ForeignKey(Gate, on_delete=models.CASCADE, null=True, related_name='exit_gates', blank=True)
+    exit_datetime = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Movement entry'
+        verbose_name_plural = 'Movement entries'
+
+    def __str__(self):
+        return f'{self.reference_number}'
+
+    def status(self):
+        if isinstance(self.exit_datetime, datetime.datetime) or isinstance(self.exit_gate, Gate):
+            return 'OUT'
+        return 'IN'
